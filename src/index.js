8187543440f2d5af709868ec3bfad51c6368eed3
Mykola Rudnev <RudnevMykola@gmail.com>
2020-05-22 15:58:57 +0200

    Set up apollo react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ApolloClient, {gql} from 'apollo-boost'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import App from './App';
+
+const client = new ApolloClient({
+  uri: 'https://react-todo-graphql-mykola.herokuapp.com/v1/graphql'
+})
+
+client.query({
+  query: gql`
+  query getTodos {
+    todos {
+      done
+      id
+      text
+    }
+  }
+  
+  `
+}).then(data => console.log(data))
+
+ReactDOM.render(<App />, document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
