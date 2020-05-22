139008a964ae0a54944cd52b7d8da278c300589c
Mykola Rudnev <RudnevMykola@gmail.com>
2020-05-22 16:54:57 +0200

    Added style with tachyons

--- src/App.js
+++ src/App.js
@@ -20,12 +20,43 @@ function App() {
   if(error) return <div>Error fatching todos</div>
 
     return (
-          <div>{data.todos.map(todo => (
-          <p>key={todo.id}
-            <span>{todo.text} </span>
-            <button> &times; </button>
+          <div className="vh-100 code flex flex-column items-center bg-purple white pa3 fl-1">
+            <h1 className="f2-1">GraphQL Cheklist{" "}
+            <span role="img" aria-label="Checkmark">✅</span>
+            </h1>
+            {/* Todo Form */}
+            <form className="mb3"> 
+              <input
+                  className="pa2 f4
+                  b--dashed"
+                  type="text"
+                  placeholder="Write youre todo"
+                  />
+                  <button
+                  className="pa2 f4 bg-green"
+                  type="submit"
+                  >Create</button>
+            </form>
+            {/* Todo List */}
+            <div className="flex items-center justify-center 
+            flex-column">
+            {data.todos.map(todo => (
+          <p key={todo.id}>
+            <span
+            className="pointer 
+            list pa1 f3"
+            >{todo.text} </span>
+            <button
+            className="bg-transpare nt bn f4">
+              <span className="red">
+                &times; 
+              </span>
+              
+            </button>
           </p>
-          ))}</div>)  
+          ))}
+          </div>
+        </div>);  
           
   return (
     <div >
