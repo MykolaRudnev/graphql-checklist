26c3406b4b3d983e72d223f364f3383b49739b62
Mykola Rudnev <RudnevMykola@gmail.com>
2020-05-23 16:17:54 +0200

    added toggle Todo and todoText

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {useQuery} from '@apollo/react-hooks'
+import React, { useState } from 'react';
+import {useQuery, useMutation} from '@apollo/react-hooks'
 import {gql} from 'apollo-boost'
 
 const GET_TODOS = gql`
@@ -12,9 +12,67 @@ query getTodos {
 }
 `
 
+const TOGGLE_TODO =gql`
+mutation toggeleTodo($id: uuid!, $done: Boolean!) {
+  update_todos(where: {id: {_eq: $id}}, _set: {done: $done}) {
+    returning {
+      done
+      id
+      text
+    }
+  }
+}
+`;
+const ADD_TODO = gql`
+mutation addTodo($text:String!){
+  insert_todos(objects: {text:$text }) {
+    returning {
+      done
+      text
+      id
+    }
+  }
+}`;
+const DELETE_TODO = gql`
+mutation deleteTodo($id: uuid!) {
+  delete_todos(where: {id: {_eq:$id }}) {
+    returning {
+      id
+      text
+      done
+    }
+  }
+}`;
+
 
 function App() {
+  const [todoText, setTodoText] = useState('')
   const {data, loading, error} = useQuery(GET_TODOS)
+  const [toggleTodo]= useMutation(TOGGLE_TODO);
+  const [addTodo] =useMutation(ADD_TODO, {
+    onCompleted: () => setTodoText("")
+  });
+  const [deleteTodo] = useMutation(DELETE_TODO)
+
+
+    async function handleToggleTodo({id, done}){
+    const data = await  toggleTodo({ variables: {id, done: !done} })
+    console.log('toggled todo',data)
+  }
+
+  async function handleAddTodo(event){
+    event.preventDefault();
+    if(!todoText.trim()) return;
+   const data = await addTodo({
+     variables: {text: todoText},
+     refetchQueries: [
+       { query: GET_TODOS}
+     ]
+    
+    });
+    console.log('added todo',data); 
+  }
+
   if(loading) return <div>loading todos..</div>
   
   if(error) return <div>Error fatching todos</div>
@@ -25,12 +83,14 @@ function App() {
             <span role="img" aria-label="Checkmark">✅</span>
             </h1>
             {/* Todo Form */}
-            <form className="mb3"> 
+            <form onSubmit={handleAddTodo} className="mb3"> 
               <input
                   className="pa2 f4
                   b--dashed"
                   type="text"
                   placeholder="Write youre todo"
+                  onChange={event => setTodoText(event.target.value)}
+                  value={todoText}
                   />
                   <button
                   className="pa2 f4 bg-green"
@@ -41,10 +101,10 @@ function App() {
             <div className="flex items-center justify-center 
             flex-column">
             {data.todos.map(todo => (
-          <p key={todo.id}>
+          <p onDoubleClick={() => handleToggleTodo(todo)} key={todo.id}>
             <span
-            className="pointer 
-            list pa1 f3"
+            className={`pointer 
+            list pa1 f3 ${todo.done && 'strike'}`}
             >{todo.text} </span>
             <button
             className="bg-transpare nt bn f4">
@@ -57,12 +117,6 @@ function App() {
           ))}
           </div>
         </div>);  
-          
-  return (
-    <div >
-      App
-    </div>
-  );
 }
 
 export default App;
